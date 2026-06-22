971d177f8a6025fc9743e56eb94c140de98f95fa
ssvibitha <vibithavarshini7@gmail.com>
2026-06-22 13:11:22 +0530

    Modified file add_RelatedToID.py

--- add_RelatedToID.py
+++ add_RelatedToID.py
@@ -2,14 +2,15 @@
 import pandas as pd
 
 df_src = pd.read_csv('data/Leads_exports.csv')
-df_dest= pd.read_csv('new_accounts.csv')
+df_dest= pd.read_csv('new_contacts.csv')
 
 
 df_dest['Lead Id'] = df_dest['Account Name'].map(
     df_src.set_index('Company')['Record Id']
 )
-
-df_dest.to_csv("accounts_9000.csv", index=False)
+df_dest = df_dest.drop("Account Id",axis=1)
+df_dest.to_csv("contacts_9000.csv", index=False)
+print(df_dest.columns)
 print(df_dest["Lead Id"].value_counts().sum())
 
 # df=pd.read_csv("new_leads_cleaned.csv")
