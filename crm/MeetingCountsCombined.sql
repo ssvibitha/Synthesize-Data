4f9cff1eaf3b179a313bd773a0c7d60451d80ebc
ssvibitha <vibithavarshini7@gmail.com>
2026-06-22 17:02:15 +0530

    Added files to combine 2 tables

---
+++ crm/MeetingCountsCombined.sql
@@ -0,0 +1,17 @@
+SELECT
+		 x.Activity_Link_ID AS Activity_Link_ID,
+		 SUM(x.NumberOfCalls) AS NumberOfMeetings
+FROM (	SELECT
+			 c."Related To" AS Activity_Link_ID,
+			 COUNT(*) AS NumberOfCalls
+	FROM  Meetings c 
+	GROUP BY  c."Related To" 
+	UNION ALL
+ 	SELECT
+			 c9."Lead ID" AS Activity_Link_ID,
+			 COUNT(*) AS NumberOfMeetings
+	FROM  Meeting_9000 c9 
+	GROUP BY  c9."Lead ID" 
+ 
+) x 
+GROUP BY  x.Activity_Link_ID 
