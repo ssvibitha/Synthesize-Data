ed0c2c57d6041547581c2e093efc293badfd3619
ssvibitha <vibithavarshini7@gmail.com>
2026-06-22 17:02:40 +0530

    Added file MLQueryTable_v3.sql

---
+++ crm/MLQueryTable_v3.sql
@@ -0,0 +1,33 @@
+SELECT
+		 l."Id" AS Lead_ID,
+		 l."Lead Source" AS Lead_Source,
+		 l."Industry" AS Lead_Industry,
+		 cm."Campaign Name" AS Campaign_Name,
+		 cm."Member Status" AS Member_Status,
+		 
+			CASE
+				 WHEN c."Last Name"  IS NOT NULL THEN 1
+				 ELSE 0
+			 END AS Converted,
+		 COALESCE(tc.NumberOfOtherTasks, 0) AS NumberOfOtherTasks,
+		 COALESCE(cc.NumberOfCalls, 0) AS NumberOfCalls,
+		 COALESCE(mc.NumberOfMeetings, 0) AS NumberOfMeetings
+FROM  Leads l
+LEFT JOIN Contacts c ON l."Last Name"  = c."Last Name" 
+LEFT JOIN Accounts a ON c."Account Name"  = a."Account Name" 
+LEFT JOIN OtherTask_counts_combined tc ON REPLACE(tc.Activity_Link_ID, 'zcrm_', '')  = 
+		CASE
+			 WHEN c."Last Name"  IS NOT NULL THEN a."Id"
+			 ELSE l."Id"
+		 END 
+LEFT JOIN CallCountsCombined cc ON REPLACE(cc.Activity_Link_ID, 'zcrm_', '')  = 
+		CASE
+			 WHEN c."Last Name"  IS NOT NULL THEN a."Id"
+			 ELSE l."Id"
+		 END 
+LEFT JOIN MeetingCountsCombined mc ON REPLACE(mc.Activity_Link_ID, 'zcrm_', '')  = 
+		CASE
+			 WHEN c."Last Name"  IS NOT NULL THEN a."Id"
+			 ELSE l."Id"
+		 END 
+LEFT JOIN "Campaign Members" cm ON REPLACE(cm."Lead ID", 'zcrm_', '')  = l."Id"  
