f92c0bd8aacebe3ff7409dfc01285d8e29379f1f
ssvibitha <vibithavarshini7@gmail.com>
2026-06-22 11:08:44 +0530

    Added file LeadID_OnlyNum.py

---
+++ LeadID_OnlyNum.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df = pd.read_csv("new_leads.csv")
+df["lead_id"] = df["lead_id"].str.replace("L", "", regex=False).astype(int)
+df = df.drop("isConverted",axis=1)
+df.to_csv("new_leads_cleaned.csv", index=False)
