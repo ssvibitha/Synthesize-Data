c228745a0a1b1cf94f6cd7b9e9430e90d2678faa
ssvibitha <vibithavarshini7@gmail.com>
2026-06-23 10:38:51 +0530

    Added comments in crm/MLQueryTable_v3.sql

--- crm/MLQueryTable_v3.sql
+++ crm/MLQueryTable_v3.sql
@@ -1,3 +1,8 @@
+-- Query table accomodating 10,000 leads and combined data of contacts, activities 
+-- due to limited crm storage
+-- includes details like lead_id, lead_source, lead_industry, campaign_name, 
+-- member_status in campaign, no.of calls,tasks,activities per lead
+
 SELECT
 		 l."Id" AS Lead_ID,
 		 l."Lead Source" AS Lead_Source,
@@ -13,7 +18,7 @@ SELECT
 		 COALESCE(cc.NumberOfCalls, 0) AS NumberOfCalls,
 		 COALESCE(mc.NumberOfMeetings, 0) AS NumberOfMeetings
 FROM  Leads l
-LEFT JOIN Contacts c ON l."Last Name"  = c."Last Name" 
+LEFT JOIN CombinedContacts c ON l."Last Name"  = c."Last Name" 
 LEFT JOIN Accounts a ON c."Account Name"  = a."Account Name" 
 LEFT JOIN OtherTask_counts_combined tc ON REPLACE(tc.Activity_Link_ID, 'zcrm_', '')  = 
 		CASE
