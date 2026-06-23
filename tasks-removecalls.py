011ada2cfa21886c7ac9170b92fb09486cf58b1a
ssvibitha <vibithavarshini7@gmail.com>
2026-06-23 15:55:04 +0530

    Removed file

--- tasks-removecalls.py
+++
@@ -1,5 +0,0 @@
-# use taske_leads to create meeting_leads, calls_leads
-import pandas as pd
-df = pd.read_csv("data/tasks_leads.csv")
-df = df[df['Subject'] != 'Call']
-df.to_csv("data/tasks_leads.csv",index=False)
