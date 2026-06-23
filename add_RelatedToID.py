c0e21669961ee6a59a8abdcc4098822ee6abdcd7
ssvibitha <vibithavarshini7@gmail.com>
2026-06-23 15:40:19 +0530

    Modified file add_RelatedToID.py

--- add_RelatedToID.py
+++ add_RelatedToID.py
@@ -1,20 +1,22 @@
 #Add account ID in contacts.csv
 import pandas as pd
 
-df_src = pd.read_csv('data/Leads_exports.csv')
-df_dest= pd.read_csv('data/new_leads.csv')
+df_src = pd.read_csv('data/Accounts_exports.csv')
+df_dest= pd.read_csv('data/contacts_9000.csv')
 
-
-df_dest['Record Id'] = df_dest['company'].map(
-    df_src.set_index('Company')['Record Id']
+df_dest['Account Id'] = df_dest['Account Name'].map(
+    df_src.set_index('Account Name')['Record Id']
 )
-df_dest.to_csv("new_leads1.csv", index=False)
+df_dest.to_csv("contacts_9.csv", index=False)
 print(df_dest.columns)
 
-# df=pd.read_csv("new_leads_cleaned.csv")
-# unique_name = df["name"].value_counts().sum() #9000
-# unique_email=df["email"].value_counts().sum()#9000
-# unique_phone = df["mobile"].value_counts().sum()#9000
-# print("unique_email: ",unique_email)
-# print("unique_name: ",unique_name)
-# print("unique_phone: ",unique_phone)
+# Use when some records are Nan in mapping column
+# mapping = (
+#     df_src
+#     .dropna(subset=['Account Id'])
+#     .set_index('Account Id')['Record Id']
+# )
+
+# df_dest['Lead ID'] = df_dest['Account Id'].map(mapping)
+
+# df_dest.to_csv("contacts_leadId.csv", index=False)
