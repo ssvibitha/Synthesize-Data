0e2726660aaa74b29e904cd77238cad538d64173
ssvibitha <vibithavarshini7@gmail.com>
2026-06-23 10:47:08 +0530

    Added file OtherTask_counts_combined.sql

---
+++ crm/OtherTask_counts_combined.sql
@@ -0,0 +1,25 @@
+-- CombinedTasks_Count - Query Table
+-- For 10,000 records
+
+SELECT
+    x.Activity_Link_ID AS Activity_Link_ID,
+    SUM(x.NumberOfOtherTasks) AS NumberOfOtherTasks
+FROM
+(
+    SELECT
+        t."Related To" AS Activity_Link_ID,
+        COUNT(*) AS NumberOfOtherTasks
+    FROM Tasks t
+    WHERE t."Subject" NOT IN ('Call', 'Meeting')
+    GROUP BY t."Related To"
+
+    UNION ALL
+
+    SELECT
+        t9."Lead ID" AS Activity_Link_ID,
+        COUNT(*) AS NumberOfOtherTasks
+    FROM Tasks_9000 t9
+    WHERE t9."Subject" NOT IN ('Call', 'Meeting')
+    GROUP BY t9."Lead ID"
+) x
+GROUP BY x.Activity_Link_ID
